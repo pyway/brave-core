1c42295cc60d1770dd529e6650493ba5af42aee1
jamesmudgett <jmm.seasons@gmail.com>
2020-04-28 14:42:28 -0500

    wip confab "Brave Together" widget

--- components/definitions/newTab.d.ts
+++ components/definitions/newTab.d.ts
@@ -71,7 +71,7 @@ declare namespace NewTab {
     url: string
   }
 
-  export type StackWidget = 'rewards' | 'binance' | ''
+  export type StackWidget = 'rewards' | 'binance' | 'together' | ''
 
   export interface LegacyState {
     pinnedTopSites: Site[]
@@ -118,6 +118,7 @@ declare namespace NewTab {
     showClock: boolean
     showTopSites: boolean
     showRewards: boolean
+    showTogether: boolean
     showBinance: boolean
     brandedWallpaperOptIn: boolean
     isBrandedWallpaperNotificationDismissed: boolean
